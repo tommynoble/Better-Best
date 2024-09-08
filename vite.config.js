bf6777f86ba74d33916145d8366a9b61b0492499
tommyculdet <youremail@example.com>
2024-09-08 16:45:20 -0400

    Remove vite-plugin-sitemap from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({
-      hostname: 'https://betterandbesthcare.com', // Replace with your actual domain
-      robotsTxt: false, // Disable robots.txt generation
-    }),
   ],
 });
