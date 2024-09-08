00fdf10e3bbafaecdb0208d9bcaefc5c4d660a0d
tommyculdet <youremail@example.com>
2024-09-08 16:08:09 -0400

    Disable robots.txt generation in vite-plugin-sitemap

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import sitemap from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      hostname: 'https://yoursite.com', // Replace with your site URL
+      hostname: 'https://betterandbesthcare.com',
+      robotsTxt: false, // Disable robots.txt generation
     }),
   ],
 });
